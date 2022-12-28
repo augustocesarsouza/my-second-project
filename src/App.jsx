1f8ea51b323a0cd031889d3c2e0ee684eb884045
augustocesarsouza <augustocesarsantana90@gmail.com>
2022-12-28 10:35:15 -0400

    att

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+//
+const Button = React.memo(({ incrementButton }) => {
+  console.log('Filho, renderizou')
+  return <button onClick={() => incrementButton(20)}>+</button>
+});
 
-class App extends Component {
-  state = {
-    reverse: false,
-  }
+function App() {
+  const [counter, setCounter] = useState(0)
 
-  handleClick = () => {
-    const { reverse } = this.state
-    this.setState({ reverse: !reverse })
-  }
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num)
+  }, [])
 
-  render() {
-    const { reverse } = this.state
-    const reverseClass = reverse ? 'reverse' : ''
+  console.log('Pai, renderizou')
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-          <button type='button' onClick={this.handleClick}>Reverse {reverseClass}</button>
-        </header>
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <p>Teste 3</p>
+      <h1>C1: {counter}
+      </h1>
+      <Button incrementButton={incrementCounter} />
+    </div>
+  )
 }
 
 export default App;
