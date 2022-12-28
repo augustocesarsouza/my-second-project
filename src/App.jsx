9cdb2c8d364a0ef188b25a79553c6342750a514d
augustocesarsouza <augustocesarsantana90@gmail.com>
2022-12-28 13:55:56 -0400

    salvar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,58 @@
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
-import React, { useState, useEffect, useCallback } from 'react';
 
-//
-const Button = React.memo(({ incrementButton }) => {
-  console.log('Filho, renderizou')
-  return <button onClick={() => incrementButton(20)}>+</button>
-});
+const Posts = ({ post, handleClick }) => {
+  console.log('Filho Renderizou')
+  return (
+    <div key={post.id} className="post">
+      <h1 style={{ fontSize: '2rem' }}
+        onClick={() => handleClick(post.title)}>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  )
+}
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+  const input = useRef(null);
 
-  const incrementCounter = useCallback((num) => {
-    setCounter((c) => c + num)
-  }, [])
+  console.log('Pai renderizou!')
 
-  console.log('Pai, renderizou')
+  // Component did mount
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(r => r.json())
+      .then(r => setPosts(r))
+  }, []);
+
+  useEffect(() => {
+    input.current.focus()
+    console.log(input.current)
+
+  }, [value]);
+
+  const handleClick = (value) => {
+    setValue(value)
+  }
 
   return (
-    <div className="App">
-      <p>Teste 3</p>
-      <h1>C1: {counter}
-      </h1>
-      <Button incrementButton={incrementCounter} />
+    <div className='App'>
+      <p>
+        <input
+          type="search"
+          ref={input}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
+        return posts.length > 0 &&
+          posts.map(post => {
+            return <Posts key={post.id} post={post} handleClick={handleClick} />
+          })
+      }, [posts])}
+      {posts.length <= 0 && (<p>Ainda não existe post</p>)}
     </div>
   )
 }
