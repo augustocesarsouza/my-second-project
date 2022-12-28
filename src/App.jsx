507a354f62db2c775237bff468367909e9871efb
augustocesarsouza <augustocesarsantana90@gmail.com>
2022-12-28 15:02:37 -0400

    salvando

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,28 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useContext } from 'react';
 import './App.css';
 
-const Posts = ({ post, handleClick }) => {
-  console.log('Filho Renderizou')
-  return (
-    <div key={post.id} className="post">
-      <h1 style={{ fontSize: '2rem' }}
-        onClick={() => handleClick(post.title)}>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
-  )
-}
+const globalState = {
+  title: 'O titulo do contexo ',
+  counter: 0,
+};
+
+const GlobalContext = React.createContext()
+
+const Div = ({ children }) => {
+  return <H1 />
+};
+
+const H1 = () => {
+  const theContext = useContext(GlobalContext);
+  return <h1>{theContext.title}</h1>
+};
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [value, setValue] = useState('');
-  const input = useRef(null);
-
-  console.log('Pai renderizou!')
-
-  // Component did mount
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(r => r.json())
-      .then(r => setPosts(r))
-  }, []);
-
-  useEffect(() => {
-    input.current.focus()
-    console.log(input.current)
-
-  }, [value]);
-
-  const handleClick = (value) => {
-    setValue(value)
-  }
-
   return (
-    <div className='App'>
-      <p>
-        <input
-          type="search"
-          ref={input}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </p>
-      {useMemo(() => {
-        return posts.length > 0 &&
-          posts.map(post => {
-            return <Posts key={post.id} post={post} handleClick={handleClick} />
-          })
-      }, [posts])}
-      {posts.length <= 0 && (<p>Ainda não existe post</p>)}
-    </div>
-  )
+    <GlobalContext.Provider value={globalState}>
+      <Div />
+    </GlobalContext.Provider>
+  );
 }
 
 export default App;
