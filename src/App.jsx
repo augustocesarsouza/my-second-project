d26385b0b035ef41939ff985ba23836717f66771
augustocesarsouza <augustocesarsantana90@gmail.com>
2022-12-29 08:22:30 -0400

    atualização separando os componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,15 @@
-import React, { useContext, useState } from "react";
 import "./App.css";
-
-const globalState = {
-    title: "O titulo do contexo ",
-    body: "O body do contexto",
-    counter: 0,
-};
-
-const GlobalContext = React.createContext();
-
-const Div = ({ children }) => {
-    return (
-        <>
-            <H1 />
-            <P />
-        </>
-    );
-};
-
-const H1 = () => {
-    const theContext = useContext(GlobalContext);
-    const {
-        contextState: { title, counter },
-    } = theContext;
-    return (
-        <h1>
-            {title} {counter}
-        </h1>
-    );
-};
-
-const P = () => {
-    const theContext = useContext(GlobalContext);
-    console.log(theContext);
-    const {
-        contextState: { body, counter },
-        contextState,
-        setContextState,
-    } = theContext;
-    return (
-        <p
-            onClick={() =>
-                setContextState((s) => ({ ...s, counter: s.counter + 1 }))
-            }
-        >
-            {body}
-        </p>
-    );
-};
+import { AppContext } from "./contexts/AppContext";
+import { Div } from "./components/Div";
 
 function App() {
-    const [contextState, setContextState] = useState(globalState);
-
-    return (
-        <GlobalContext.Provider value={{ contextState, setContextState }}>
-            <Div />
-        </GlobalContext.Provider>
-    );
+  return (
+    <div className="editMinha">
+      <AppContext>
+        <Div />
+      </AppContext>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/contexts/AppContext/data.js
@@ -0,0 +1,7 @@
+export const globalState = {
+  title: "BLA BLA BLA ",
+  body: "O paragrafo",
+  counter: 100,
+};
+
+export default globalState
